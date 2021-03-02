93c141fef5f3fc4400ff8067be224ebeaebad6f4
Harry Burgess <harry@harrys-air.home>
2021-03-02 13:45:31 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Random Film
+
+Provides a random selection from a Letterboxd watchlist csv.
+
+Future plans incliude:
+ - Ability to enter Letterboxd username and password and the rest is automatic.
+ - GUI
+ - Filters for genre, release date, rating, length
---
+++ random_film.py
@@ -0,0 +1,56 @@
+import random
+import csv
+import requests
+from bs4 import BeautifulSoup
+import webbrowser
+import os
+
+
+def pick_film():
+    html_output = ""
+
+    with open("watchlist.csv", "r") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        total_list = list(csv_reader)
+        r_num = random.randint(1, len(total_list))
+        selected_film = total_list[r_num - 1]
+
+    title = selected_film['Name']
+    year = selected_film['Year']
+    URL = selected_film['Letterboxd URI']
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    poster = soup.find(attrs={"alt": selected_film['Name']})["src"]
+    director = soup.find(id="featured-film-header").p.span.text
+    runtime = soup.find(attrs={"class": "text-footer"}).text.strip().split("mins", 1)[0]
+    runtime_hours = f"{int(runtime) // 60} hours {int(runtime) % 60} minutes"
+
+    pb_link = f"https://thepiratebay.org/search.php?q={(selected_film['Name'] + '+' + selected_film['Year']).replace(' ', '+')}&all=on&search=Pirate+Search&page=0&orderby="
+    jw_link = f"https://www.justwatch.com/uk/search?q={selected_film['Name'].replace(' ', '%20')}"
+
+    html_output += f"\t\t<h1>{title} ({year})</h1>\n"
+    html_output += f"\t\t<div class=\"frame\"><img src=\"{poster}\" alt=\"\"></div>\n"
+    html_output += f"\t\t<span>Directed by {director} — {runtime_hours}</span>\n"
+    html_output += f"\t\t<div class=\"links\">\n"
+    html_output += f"\t\t\t<a href=\"{URL}\" target=\"_blank\">View on Letterboxd</a>\n"
+    html_output += f"\t\t\t<a href=\"{jw_link}\" target=\"_blank\">How to watch</a>\n"
+    html_output += f"\t\t\t<a href=\"{pb_link}\" target=\"_blank\">TPB</a>\n"
+    html_output += f"\t\t</div>\n"
+    html_output += f"\t</div>\n"
+    html_output += f"</body>\n"
+    html_output += f"</html>"
+
+    index = "site-files/index.html"
+
+    with open('site-files/before.html', 'r', encoding='utf-8') as before:
+        before = before.read()
+
+    with open(index, 'w') as filetowrite:
+        filetowrite.write(before + html_output)
+
+    webbrowser.open('file://' + os.path.realpath(index), new=2)
+
+
+pick_film()
